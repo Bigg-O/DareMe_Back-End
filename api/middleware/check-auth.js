9d8dccb6515558e207fed71540d9bb31a010960f
Wookeun Song <wookeun.s@gmail.com>
2020-02-04 20:08:48 -0600

    changes in auth

--- api/middleware/check-auth.js
+++ api/middleware/check-auth.js
@@ -2,9 +2,9 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
     try {
-        const token = req.headers.authentication.split(" ")[1];
+        const token = req.headers.authorization.split(" ")[1];
         const decoded = jwt.verify(token, process.env.JWT_KEY);
-        req.userData = decoded;
+        req.body.userData = decoded;
         next();
     } catch (error) {
         return res.status(401).json({
